1e7b5def5d147b52db2e291ea29ae22296991b93
alamincse97 <alamin.cse97@gmail.com>
2025-10-28 14:05:40 +0600

    [ADD] express intall

---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+    try{
+        app.listen(PORT, () => {
+            logger.info(`Server running on port ${PORT}`);
+        });
+    }
+    catch (error){
+        console.log("Error...");
+    }
+}
