e586abd26418fced3e9c0c0a76fdf0849ded041f
alamincse97 <alamin.cse97@gmail.com>
2025-11-16 12:14:58 +0600

    Added pg

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 require("dotenv").config();
 const app = require("./app");
 const logger = require("./logger");
+const pool = require('./config/db');
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     try{
+        await pool.query(`
+            CREATE TABLe IF NOT EXISTS users {
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+                 `);
         app.listen(PORT, () => {
             logger.info(`Server running on port ${PORT}`);
         });
--- services/userService.js
+++ services/userService.js
@@ -1,9 +1,11 @@
 const {log} = require("console");
+const cache = require('../utills/cache');
 
 class UserService {
 
     createUser(data) {
         console.log("Creating user", data);
+        cache.set('users', data, 60);
     }
 }
 
